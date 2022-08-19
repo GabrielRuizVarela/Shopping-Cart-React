742ba6ecf3ec363d740fbba6b10cec769974c08d
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-19 16:27:10 -0300

    Completed footer tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import githubSVG from '../images/github_social.svg';
+import '../styles/SocialButton.scss';
+
+function SocialButton() {
+  return (
+    <footer className="author-footer">
+      <a href="https://github.com/GabrielRuizVarela">
+        <p className="social-text">
+          Createad By Gabriel Ruiz Varela
+        </p>
+      </a>
+      <a href="https://github.com/GabrielRuizVarela" className="social github">
+        <img src={githubSVG} alt="Github social button" />
+      </a>
+    </footer>
+  );
+}
+
+export default SocialButton;
