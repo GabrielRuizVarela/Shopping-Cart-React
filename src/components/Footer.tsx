14fe33eb36e22bbaea92a9cc60c4d044664ddffc
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-22 08:47:39 -0300

    Change SocialButton.scss to Footer.scss

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import githubSVG from '../images/github_social.svg';
-import '../styles/SocialButton.scss';
+import '../styles/Footer.scss';
 
 function SocialButton() {
   return (
     <footer className="author-footer">
       <a href="https://github.com/GabrielRuizVarela">
-        <p className="social-text">
-          Createad By Gabriel Ruiz Varela
-        </p>
+        <p className="social-text">Createad By Gabriel Ruiz Varela</p>
       </a>
       <a href="https://github.com/GabrielRuizVarela" className="social github">
         <img src={githubSVG} alt="Github social button" />
