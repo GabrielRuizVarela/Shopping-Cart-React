e8b5007ae3f63702f5cbcfb2a5d4fb4726cb084e
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-23 18:24:02 -0300

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
   entry: './src/index.tsx',
