786b772e447addb44a202487efd620505a53ecf6
Gabriel Ruiz Varela <gabrielruizvarela@gmail.com>
2022-08-23 18:40:01 -0300

    Switch to HashRouter

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import React from 'react';
 import Home from '../pages/Home';
 import Shop from '../pages/Shop';
@@ -6,13 +6,13 @@ import ItemDetail from '../pages/ItemDetail';
 
 function App() {
   return (
-    <BrowserRouter basename="/Shopping-Cart-React" >
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/shop/:id" element={<ItemDetail />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
